ede1e40cb1e8a9b2ca747f74c8aa7203f0e3eddc
vikram shah <89067906+vikramshah@users.noreply.github.com>
2022-11-14 15:09:25 +0530

    - Added logo to homescreen

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -6,14 +7,8 @@
  * @flow strict-local
  */
 
-import React, { Component } from 'react';
-import {
-  Button,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from 'react-native';
+import React, {Component} from 'react';
+import {Button, Image, StyleSheet, Text, TextInput, View} from 'react-native';
 
 const styles = StyleSheet.create({
   // ZegoEasyExample
@@ -26,6 +21,17 @@ const styles = StyleSheet.create({
   },
   hidePage: {
     display: 'none',
+  },
+  logoContainer: {
+    textAlign: 'center',
+    fontSize: 24,
+    fontWeight: '600',
+    marginTop: '50%',
+    marginBottom: 100,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   logo: {
     textAlign: 'center',
@@ -52,21 +58,29 @@ const styles = StyleSheet.create({
   inputBox: {
     width: '50%',
     height: 50,
-  }
+  },
+  logoImg: {
+    height: 80,
+    width: 80,
+    backgroundColor: 'black',
+    borderRadius: 1000,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
 
 export default class HomePage extends Component {
   currentUserID;
   currentUserName;
-  currentUserIcon = "https://img.icons8.com/color/48/000000/avatar.png"; // TODO for test only
+  currentUserIcon = 'https://img.icons8.com/color/48/000000/avatar.png'; // TODO for test only
   serverUrl;
   appData;
   state = {
-    targetUserID: ''
-  }
+    targetUserID: '',
+  };
   constructor(props) {
     super(props);
-    console.log('Home constructor: ', props)
+    console.log('Home constructor: ', props);
     this.appData = props.route.params.appData;
     this.currentUserID = this.appData.userID;
     this.currentUserName = this.currentUserID.toUpperCase(); // TODO user name for test only
@@ -77,32 +91,35 @@ export default class HomePage extends Component {
     }
   }
   // Call by Routes's instance which would be trigger in the APP component by user click the notification
-  handleIncomingCall(roomID)
-  {
+  handleIncomingCall(roomID) {
     this.jumpToCallPage(roomID);
-    console.log("Handle incoming call with room id: ",roomID)
+    console.log('Handle incoming call with room id: ', roomID);
   }
   // Post a request to backend service will the targetUserID
-  // Because every device(FCM token) has been binding with a specific user id at APP launched, 
+  // Because every device(FCM token) has been binding with a specific user id at APP launched,
   // so the server can find out who you are trying to call
   async sendCallInvite(roomID) {
     const requestOptions = {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
         targetUserID: this.state.targetUserID,
         callerUserID: this.currentUserID,
         callerUserName: this.currentUserName,
         callerIconUrl: this.currentUserIcon,
         roomID: roomID,
-        callType: 'Video' // TODO For test only
-      })
+        callType: 'Video', // TODO For test only
+      }),
     };
     const reps = await fetch(`${this.serverUrl}/call_invite`, requestOptions);
     console.log('Send call invite reps: ', reps);
   }
   jumpToCallPage(roomID) {
-    this.props.navigation.navigate('CallPage', { appData: this.appData, roomID: roomID, userName: this.currentUserName });
+    this.props.navigation.navigate('CallPage', {
+      appData: this.appData,
+      roomID: roomID,
+      userName: this.currentUserName,
+    });
   }
   // Start call by click the call button
   startCall() {
@@ -117,14 +134,26 @@ export default class HomePage extends Component {
 
   render() {
     return (
-      <View
-        style={[
-          styles.homePage,
-          styles.showPage,
-        ]}>
-        <Text style={styles.logo}>ZEGOCLOUD</Text>
-        <View style={[styles.container, { flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center' }]}>
-          <View style={[styles.inputBox, styles.container, { flexDirection: 'row' }]}>
+      <View style={[styles.homePage, styles.showPage]}>
+        {/* App logo */}
+        <View style={styles.logoContainer}>
+          <View style={styles.logoImg}>
+            <Image source={require('../img/applogo.png')} />
+          </View>
+          <Text style={{fontSize: 24, fontWeight: '600'}}>MyChat App</Text>
+        </View>
+        {/* App body  */}
+        <View
+          style={[
+            styles.container,
+            {
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            },
+          ]}>
+          <View
+            style={[styles.inputBox, styles.container, {flexDirection: 'row'}]}>
             <Text style={[styles.text]}>Your ID: </Text>
             <TextInput
               style={styles.input}
@@ -134,18 +163,20 @@ export default class HomePage extends Component {
             />
           </View>
 
-          <View style={[styles.inputBox, styles.container, { flexDirection: 'row' }]}>
+          <View
+            style={[styles.inputBox, styles.container, {flexDirection: 'row'}]}>
             <TextInput
               style={styles.input}
-              onChangeText={(text) => { this.setState({ targetUserID: text }) }}
-              placeholder='Target ID'
+              onChangeText={text => {
+                this.setState({targetUserID: text});
+              }}
+              placeholder="Target ID"
             />
             <View style={styles.ctrlBtn}>
               <Button onPress={this.startCall.bind(this)} title="📞" />
             </View>
           </View>
         </View>
-
       </View>
     );
   }
